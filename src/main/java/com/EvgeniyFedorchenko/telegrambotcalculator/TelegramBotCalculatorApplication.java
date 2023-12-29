6c0fdcdddaf8df4f0146d007e0821b708811d37a
Enot272 <jecky432@gmail.com>
2023-12-29 21:44:08 +0300

    Initial bot setup

--- src/main/java/com/EvgeniyFedorchenko/telegrambotcalculator/TelegramBotCalculatorApplication.java
+++ src/main/java/com/EvgeniyFedorchenko/telegrambotcalculator/TelegramBotCalculatorApplication.java
@@ -1,12 +1,22 @@
-package com.EvgeniyFedorchenko.telegrambotcalculator;
+package com.evgeniyfedorchenko.telegrambotcalculator;
 
+import com.evgeniyfedorchenko.telegrambotcalculator.service.TelegramBot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @SpringBootApplication
 public class TelegramBotCalculatorApplication {
     public static void main(String[] args) {
         SpringApplication.run(TelegramBotCalculatorApplication.class, args);
     }
-
+    @Bean
+    TelegramBotsApi telegramBotsApi(TelegramBot telegramBot) throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        telegramBotsApi.registerBot(telegramBot);
+        return telegramBotsApi;
+    }
 }
