c101cb2c8999ca3bc4fb31ed4092dd4f30c2016d
Enot272 <jecky432@gmail.com>
2023-12-29 03:21:52 +0300

    Initial commit

---
+++ src/main/java/com/EvgeniyFedorchenko/telegrambotcalculator/TelegramBotCalculatorApplication.java
@@ -0,0 +1,12 @@
+package com.EvgeniyFedorchenko.telegrambotcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotCalculatorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramBotCalculatorApplication.class, args);
+    }
+
+}
